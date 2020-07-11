293918f36d4cfb63e7905671aac77ecec797dbf5
unknown <guivecchi12@gmail.com>
2020-07-10 19:46:43 -0600

    Start

---
+++ friends/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/protected" />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    // make a post request to the login endpoint on the server
+    axios
+      .post("http://localhost:3000/login", this.state.credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        // redirect the user to the app's main logged in page
+        this.props.history.push("/protected");
+      })
+      .catch(err => console.log({ err }));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
